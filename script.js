8f7357d7861b4be5f70ec6957c6357a3478b1558
20essentials <s0l0estudi4r@gmail.com>
2024-06-18 16:06:35 +0200

    v0.0.1

---
+++ script.js
@@ -0,0 +1,7 @@
+console.groupCollapsed("Reference", "https://youtu.be/xDTiPkQeR2M?si=cenrqOyeGmjlfK3u");
+console.log("Image 1", "https://wallhaven.cc/w/952qxx");
+console.log("Image 2", "https://wallhaven.cc/w/4935ow");
+console.log("Image 3", "https://wallhaven.cc/w/od95v7");
+console.log("Image 4", "https://wallhaven.cc/w/49gq2k");
+console.log("Image 5", "https://wallhaven.cc/w/4xyj7v");
+console.groupEnd();
